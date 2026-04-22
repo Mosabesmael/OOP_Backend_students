94a4bbd29c8bb56bb93bf4f97505579213bc27b4
Mosabesmael <mosabesmael123456789@gmail.com>
2026-04-22 23:19:22 +0200

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Cache;
+using System.Text;
+
+namespace Students
+{
+    internal class Student
+    {
+
+        private int Age;
+        private string Name;
+        private int Grade;
+        public void Student()
+        {
+
+        }
+
+    }
+}
